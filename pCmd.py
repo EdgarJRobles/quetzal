f72a957f6f159b91191f05c60cc872d8c8b984d5
hasecilu <hasecilu@tuta.io>
2024-10-23 13:06:06 -0600

    Don't translate objects' internal name
    The procedure for translation should be to translate the string that is
    assigned to the label: `obj.Label`

--- pCmd.py
+++ pCmd.py
@@ -132,9 +132,7 @@ def portsDir(o):
 
 def simpleSurfBend(path=None, profile=None):
     "select the centerline and the O.D. and let it sweep"
-    curva = FreeCAD.activeDocument().addObject(
-        "Part::Feature", translate("Part::Feature", "Simple curve")
-    )
+    curva = FreeCAD.activeDocument().addObject("Part::Feature", "Simple curve")
     if path == None or profile == None:
         curva.Shape = Part.makeSweepSurface(*fCmd.edges()[:2])
     elif path.ShapeType == profile.ShapeType == "Edge":
@@ -158,9 +156,7 @@ def makePipe(propList=[], pos=None, Z=None):
         pos = FreeCAD.Vector(0, 0, 0)
     if Z == None:
         Z = FreeCAD.Vector(0, 0, 1)
-    a = FreeCAD.ActiveDocument.addObject(
-        "Part::FeaturePython", translate("Part::FeaturePython", "Tube")
-    )
+    a = FreeCAD.ActiveDocument.addObject("Part::FeaturePython", "Tube")
     if len(propList) == 4:
         pFeatures.Pipe(a, *propList)
     else:
@@ -248,9 +244,7 @@ def makeElbow(propList=[], pos=None, Z=None):
         pos = FreeCAD.Vector(0, 0, 0)
     if Z == None:
         Z = FreeCAD.Vector(0, 0, 1)
-    a = FreeCAD.ActiveDocument.addObject(
-        "Part::FeaturePython", translate("Part::FeaturePython", "Elbow")
-    )
+    a = FreeCAD.ActiveDocument.addObject("Part::FeaturePython", "Elbow")
     if len(propList) == 5:
         pFeatures.Elbow(a, *propList)
     else:
@@ -437,9 +431,7 @@ def makeFlange(propList=[], pos=None, Z=None):
         pos = FreeCAD.Vector(0, 0, 0)
     if Z == None:
         Z = FreeCAD.Vector(0, 0, 1)
-    a = FreeCAD.ActiveDocument.addObject(
-        "Part::FeaturePython", translate("Part::FeaturePython", "Flange")
-    )
+    a = FreeCAD.ActiveDocument.addObject("Part::FeaturePython", "Flange")
     if len(propList) >= 8:
         pFeatures.Flange(a, *propList)
     else:
@@ -537,9 +529,7 @@ def makeReduct(propList=[], pos=None, Z=None, conc=True):
         pos = FreeCAD.Vector(0, 0, 0)
     if Z == None:
         Z = FreeCAD.Vector(0, 0, 1)
-    a = FreeCAD.ActiveDocument.addObject(
-        "Part::FeaturePython", translate("Part::FeaturePython", "Reduction")
-    )
+    a = FreeCAD.ActiveDocument.addObject("Part::FeaturePython", "Reduction")
     propList.append(conc)
     pFeatures.Reduct(a, *propList)
     a.ViewObject.Proxy = 0
@@ -566,9 +556,7 @@ def makeUbolt(propList=[], pos=None, Z=None):
         pos = FreeCAD.Vector(0, 0, 0)
     if Z == None:
         Z = FreeCAD.Vector(0, 0, 1)
-    a = FreeCAD.ActiveDocument.addObject(
-        "Part::FeaturePython", translate("Part::FeaturePython", "U-Bolt")
-    )
+    a = FreeCAD.ActiveDocument.addObject("Part::FeaturePython", "U-Bolt")
     if len(propList) == 5:
         pFeatures.Ubolt(a, *propList)
     else:
@@ -589,9 +577,7 @@ def makeShell(L=1000, W=1500, H=1500, thk1=6, thk2=8):
       H(eight):        default=500
       thk (thickness): default=6
     """
-    a = FreeCAD.ActiveDocument.addObject(
-        "Part::FeaturePython", translate("Part::FeaturePython", "Tank")
-    )
+    a = FreeCAD.ActiveDocument.addObject("Part::FeaturePython", "Tank")
     pFeatures.Shell(a, L, W, H, thk1, thk2)
     a.ViewObject.Proxy = 0
     a.Placement.Base = FreeCAD.Vector(0, 0, 0)
@@ -617,9 +603,7 @@ def makeCap(propList=[], pos=None, Z=None):
         pos = FreeCAD.Vector(0, 0, 0)
     if Z == None:
         Z = FreeCAD.Vector(0, 0, 1)
-    a = FreeCAD.ActiveDocument.addObject(
-        "Part::FeaturePython", translate("Part::FeaturePython", "Cap")
-    )
+    a = FreeCAD.ActiveDocument.addObject("Part::FeaturePython", "Cap")
     if len(propList) == 3:
         pFeatures.Cap(a, *propList)
     else:
@@ -1247,9 +1231,7 @@ def makeValve(propList=[], pos=None, Z=None):
         pos = FreeCAD.Vector(0, 0, 0)
     if Z == None:
         Z = FreeCAD.Vector(0, 0, 1)
-    a = FreeCAD.ActiveDocument.addObject(
-        "Part::FeaturePython", translate("Part::FeaturePython", "Valve")
-    )
+    a = FreeCAD.ActiveDocument.addObject("Part::FeaturePython", "Valve")
     if len(propList):
         pFeatures.Valve(a, *propList)
     else:
@@ -1441,9 +1423,7 @@ def makeNozzle(DN="DN50", H=200, OD=60.3, thk=3, D=160, d=62, df=132, f=14, t=15
 def makeRoute(n=Z):
     curvedEdges = [e for e in fCmd.edges() if e.curvatureAt(0) != 0]
     if curvedEdges:
-        s = FreeCAD.ActiveDocument.addObject(
-            "Sketcher::SketchObject", translate("Sketcher::SketchObject", "pipeRoute")
-        )
+        s = FreeCAD.ActiveDocument.addObject("Sketcher::SketchObject", "pipeRoute")
         s.MapMode = "SectionOfRevolution"
         sup = fCmd.edgeName()
         s.Support = [sup]
