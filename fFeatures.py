fd1e0f4fcebb7f470d6efecfd5daa8be32c66c53
hasecilu <hasecilu@tuta.io>
2025-01-13 18:18:08 -0600

    Use PySide instead of PySide2

--- fFeatures.py
+++ fFeatures.py
@@ -16,9 +16,9 @@ import FreeCADGui
 import Part
 from Arch import makeProfile, makeStructure
 from Draft import makeCircle
-from PySide2.QtCore import *
-from PySide2.QtGui import *
-from PySide2.QtWidgets import (
+from PySide.QtCore import *
+from PySide.QtGui import *
+from PySide.QtWidgets import (
     QCheckBox,
     QComboBox,
     QDialog,
